3c1ee291385628040782198b98ecbf6e2335d57a
EricLam404 <lameric9888@gmail.com>
2022-01-15 19:41:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Discord = require('discord.js');
 const { Client, Intents } = require('discord.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-client.login(process.env.BOTTOKEN);
+client.login(OTE3NTUxMDMzOTczMDgwMTM0.Ya6V_Q.zPmFfMZDe4y5RPWiDbAe-vS6VDQ);
 
 client.on('ready', readyDiscord);
 
