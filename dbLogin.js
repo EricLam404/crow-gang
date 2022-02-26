cfd5ce2a192d3e9da57786aa6736f02edb87bf8f
EricLam404 <lameric9888@gmail.com>
2022-02-25 19:08:39 -0500

    Update dbLogin.js

--- dbLogin.js
+++ dbLogin.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 module.exports = async () => {
   try {
     let connect = process.env.dbToken;
-    await mongoose.connect(connect));
+    await mongoose.connect(connect);
     console.log('Database Connected Successfully');
   } catch (error) {
     console.log('Database Failed to Connect: ', error);
